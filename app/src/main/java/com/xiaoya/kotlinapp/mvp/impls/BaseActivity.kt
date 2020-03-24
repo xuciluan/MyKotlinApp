c80721f5696532c6c5558951b1b896174c6e7170
xuciluan <xuciluan@126.com>
2020-03-24 09:26:08 +0800

    提交BaseActivity

---
+++ app/src/main/java/com/xiaoya/kotlinapp/mvp/impls/BaseActivity.kt
@@ -0,0 +1,68 @@
+package com.xiaoya.kotlinapp.mvp.impls
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.xiaoya.kotlinapp.mvp.IMvpView
+import com.xiaoya.kotlinapp.mvp.IPresenter
+import java.lang.reflect.ParameterizedType
+import java.lang.reflect.Type
+import kotlin.reflect.KClass
+import kotlin.reflect.full.isSubclassOf
+import kotlin.reflect.full.primaryConstructor
+import kotlin.reflect.jvm.jvmErasure
+
+/**
+ * @author : xuciluan@126.com
+ * @date: 2020-03-24
+ * @desc:
+ **/
+
+abstract class BaseActivity<out P:BasePresenter<BaseActivity<P>>>(contentLayoutId: Int) : IMvpView<P> ,AppCompatActivity(contentLayoutId) {
+
+    override val presenter : P
+
+    init {
+        presenter = createPresenterkt()
+        presenter.view = this
+    }
+
+    //todo : 什么是星投影？这段代码什么意思？？？
+    fun createPresenterkt():P {
+        sequence{
+            var thisClass: KClass<*> = this@BaseActivity::class
+            while(true){
+                yield(thisClass.supertypes)
+                thisClass = thisClass.supertypes.firstOrNull()?.jvmErasure ?: break;
+            }
+        }.flatMap{
+            it.flatMap{
+                it.arguments
+            }.asSequence()
+        }.first{
+            it.type?.jvmErasure?.isSubclassOf(IPresenter::class)?:false
+        }.let{
+            return it.type!!.jvmErasure.primaryConstructor!!.call() as P
+        }
+    }
+
+    fun createPresenterJava():P{
+        sequence<Type>{
+            var thisClass:Class<*> = this@BaseActivity.javaClass
+            while(true){
+                yield(thisClass.genericSuperclass)
+                thisClass = thisClass.superclass?:break
+            }
+        }.filter {
+            it is ParameterizedType
+        }.flatMap {
+            (it as ParameterizedType).actualTypeArguments.asSequence()
+        }.first{
+            it is Class<*> && IPresenter::class.java.isAssignableFrom(it)
+        }.let {
+            return (it as Class<P>).newInstance()
+        }
+    }
+
+    //TODO 重写生命周期方法
+}
